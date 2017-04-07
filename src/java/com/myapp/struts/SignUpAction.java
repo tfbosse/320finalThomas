3c9a0bdee1b1327fe4dbbc58d483246454f9bc85
tfbosse <tfbosse97@gmail.com>
2017-04-07 15:57:27 -0500

    error checking

--- src/java/com/myapp/struts/SignUpAction.java
+++ src/java/com/myapp/struts/SignUpAction.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//figuring this shit out
 package com.myapp.struts;
 
 import java.sql.Connection;
