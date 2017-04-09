2c3bfa7d95f2cf32d1d162d17d6ed78334ed507d
jsotey <jsotey@crimson.ua.edu>
2017-04-09 18:14:20 -0500

    jake - working on customeDAO

--- src/java/com/myapp/struts/customerDAO.java
+++ src/java/com/myapp/struts/customerDAO.java
@@ -4,12 +4,13 @@
  * and open the template in the editor.
  */
 package com.myapp.struts;
-
+import java.sql.Timestamp;
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import static javax.management.Query.value;
 
 /**
  *
@@ -21,25 +22,52 @@ public class customerDAO {
 
     }
 
-    public void insertCustomer(SignUpForm cust, address addy) throws Exception {
+    public void insertCustomer(SignUpForm cust, AddressForm addy) throws Exception {
         System.out.println("jdbc connection");
         DBConnectionUtil DBcon = new DBConnectionUtil();
         Connection con = DBcon.getConnection();
         try {
 
             try {
-                PreparedStatement st = con.prepareStatement("INSERT INTO customer(store_id,first_name,last_name,email,QUANTITY,SHIPPING_COST,SALES_DATE,SHIPPING_DATE)"
+                PreparedStatement st = con.prepareStatement("INSERT INTO customer(store_id,first_name,last_name,email,active,create_date,last_update)"
                         + "VALUES(?,?,?,?,?,?,?)");
                 
+                String storeID = cust.getStoreID();
+                String first = cust.getFirstname();
+                String last = cust.getLastname();
+                String email = cust.getEmail();
+                Timestamp ts = new Timestamp(System.currentTimeMillis());
+                Date date = new Date(ts.getTime());
                 
-                st.setInt(1, (int) orderNum);
-                st.setInt(2, (int) customerID);
-                st.setInt(3, (int) productID);
-                st.setInt(4, (int) quantity);
-                st.setInt(5, (int) shippingCost);
-                st.setDate(6, salesDate);
-                st.setDate(7, shippingDate);
+                        
+                st.setInt(1, Integer.parseInt(storeID));
+                st.setString(2, first);
+                st.setString(3, last);
+                st.setString(4, email);
+                st.setInt(5, 1);
+                st.setDate(6, date);
+                st.setDate(7, date);
                 st.executeUpdate();
+                
+                PreparedStatement st1 = con.prepareStatement("INSERT INTO address(address,address2,district,city_id, postal_code,phone,last_update)"
+                        + "VALUES(?,?,?,?,?,?,?)");
+                
+                String a = addy.getAddress();
+                String a2 = addy.getAddress2();
+                String dist = addy.getDistrict();
+                String cityID = addy.getCity_id();
+                String postal = addy.getPostal_code();
+                String phone = addy.getPhone();
+                
+                
+                st1.setString(1, a);
+                st1.setString(2, a2);
+                st1.setString(3, dist);
+                st1.setInt(4, Integer.parseInt(cityID));
+                st1.setString(5, postal);
+                st1.setString(6, phone);
+                st1.setDate(7, date);
+                
                 System.out.println("1 row affected ");
             } catch (SQLException ex) {
                 System.out.println("SQL statement is not executed!" + ex);
