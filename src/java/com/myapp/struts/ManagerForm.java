a91e02b9cfccb81d7ca674370cd444a49c25dcb1
apwhite195 <apwhite@crimson.ua.edu>
2017-04-10 22:10:16 -0500

    Aidan - ManagerForm.java changed so the page shows up

--- src/java/com/myapp/struts/ManagerForm.java
+++ src/java/com/myapp/struts/ManagerForm.java
@@ -12,73 +12,67 @@ package com.myapp.struts;
 public class ManagerForm extends org.apache.struts.action.ActionForm {
     
     String id;
-    String first_name;
-    String last_name;
+    String firstname;
+    String lastname;
     String email;
     String username;
     String password;
-      
-    
-    public ManagerForm (String id, String first_name, String last_name, String email, String username, String password){
-        this.id=id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.username = username;
-        this.password = password;     
-    }
+
    
     public ManagerForm (){
-        
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+        super();
     }
 
     public String getId() {
         return id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    
     
     
 }
